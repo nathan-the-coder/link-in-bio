549b5a2edd8a303d844c9abb99c992294f304190
Nathaniel Ramos <nathanielramos726@gmail.com>
2026-05-08 14:25:27 +0800

    Fixed Leaked Firebase secrets by adding support for .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-
+import { useEffect, useState } from "react"
+import { db } from './utils/firebase.js'
 
 function App() {
+  const [hasFirebase, setHasFirebase] = useState(false);
+
+  useEffect(() => {
+    if (db) {
+      setHasFirebase(true);
+    }
+  }, [setHasFirebase]);
+
   return (
     <div>
       <h1>Hello World</h1>
+      {hasFirebase ? (
+        <h1>Firebase Connected</h1>
+      )
+        :
+        <h1>Firebase not Connected</h1>
+      }
     </div>
   )
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhUBeMNZ0QUwnGk93tYO9jHhB7YYFTzqY",
-  authDomain: "linkinbio-179c4.firebaseapp.com",
-  projectId: "linkinbio-179c4",
-  storageBucket: "linkinbio-179c4.firebasestorage.app",
-  messagingSenderId: "837991089150",
-  appId: "1:837991089150:web:5d059bea680e9bcfa036af",
-  measurementId: "G-KNFDR50EMD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
